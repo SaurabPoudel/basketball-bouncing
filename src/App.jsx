34c6322223ee86bd38069fe02923a90348ec3328
SaurabPoudel <poudelsaurab20@gmail.com>
2023-05-28 19:38:53 +0545

    Basketball bouncing done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+        <body>
+        <div className="container">
+            <div className="circle"></div>
+            <div className="floor"></div>
+        </div>
+        </body>
+
+    </>
+  );
+}
+
+export default App;
